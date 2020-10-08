e9ac21320b49d1e98b8dae26328f2ec8f6722517
Robin Baumann <mail@robin-baumann.com>
2020-10-08 11:19:57 +0200

    add qat colab demo; TF OD API labelmap

--- scripts/train.py
+++ scripts/train.py
@@ -16,6 +16,20 @@ def main():
       as_supervised=True
   )
 
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+      tf.config.experimental.set_virtual_device_configuration(
+          gpus[0],
+          [tf.config.experimental.VirtualDeviceConfiguration(
+            memory_limit=8192)])
+      logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+      print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+      # Virtual devices must be set before GPUs have been initialized
+      print(e)
+
   normalize = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
 
   def prepare(ds, shuffle=False, batch_size=32, num_classes=196):
